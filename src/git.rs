aa422ae24c811b8136c0102f7b2870989b7c20dc
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-11-16 08:58:30 -0600

    add/enhance commands
    - implement Last & Show commands
    - add All subcommand for Restore & Unstage
    - modify Undo's args just the number of commits to undo
    - added COLOR const in Git mod for "--color=always" flag
    - modify Ll command to use StdoutLock for writes
    - use GitCommand struct in private Git impl functions
    - add --print-command flag

--- src/git.rs
+++ src/git.rs
@@ -2,14 +2,24 @@ use crate::print::Print;
 use anyhow::{anyhow, Context, Result};
 use log::debug;
 use std::{
-    io::{self, Write},
+    io::{self, StdoutLock, Write},
     process::Command,
+    sync::atomic::AtomicBool,
 };
+
+pub(crate) static PRINT_COMMAND: AtomicBool = AtomicBool::new(false);
 
 pub(crate) struct Git();
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct DefaultMaxCount(u8);
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct GitCommand<'a> {
+    subcommand: &'a str,
+    default_args: &'a [&'a str],
+    user_args: &'a [String],
+}
 
 /// Outcome of running a Git command; used to set exit code at end
 #[derive(Debug)]
@@ -19,6 +29,9 @@ pub(crate) enum GitCommandResult {
 }
 
 type GitResult = Result<GitCommandResult>;
+
+/// this will force color, but isatty() will still be false
+const COLOR: &str = "--color=always"; // this will force color, but isatty() will still be false
 
 impl Git {
     /// list configured aliases, optionally filtering on those containing `filter`
@@ -34,16 +47,18 @@ impl Git {
 
         match output.status.success() {
             true => {
-                let print_fn: fn(&str) = Print::blue;
+                let output = String::from_utf8(output.stdout)?;
+                let output_lines = output.lines();
 
-                let stdout = String::from_utf8(output.stdout)?;
-                let stdout = stdout.lines();
+                let mut lock: io::StdoutLock<'_> = io::stdout().lock();
+
+                let print_fn: fn(&str, &mut StdoutLock) = Print::blue_stdout;
 
                 match filter {
                     Some(f) => {
                         let term_upper = f.to_uppercase();
 
-                        stdout
+                        output_lines
                             .filter_map(|line| {
                                 if line.to_uppercase().contains(&term_upper) {
                                     Some(line.replace("alias.", ""))
@@ -51,12 +66,12 @@ impl Git {
                                     None
                                 }
                             })
-                            .for_each(|x| print_fn(&x));
+                            .for_each(|x| print_fn(&x, &mut lock));
                     }
                     None => {
-                        stdout
+                        output_lines
                             .map(|line| line.replace("alias.", ""))
-                            .for_each(|x| print_fn(&x));
+                            .for_each(|x| print_fn(&x, &mut lock));
                     }
                 }
             }
@@ -71,15 +86,31 @@ impl Git {
         }
     }
 
-    pub(crate) fn ll(args: &[String]) -> Result<GitCommandResult> {
-        debug!("_ll_ called with: {:#?}", args);
+    pub(crate) fn last(args: &[String]) -> GitResult {
+        debug!("_last_ called with: {:#?}", args);
+
         parse_for_max_count_and_execute(
-            "log",
-            &[
-                "--color=always", // this will force color, but isatty() will still be false
-                "--pretty='%C(yellow)%h %C(magenta)%as %C(blue)%aL %C(cyan)%s%C(reset)'",
-            ],
-            args,
+            GitCommand {
+                subcommand: "log",
+                default_args: &[COLOR, "--compact-summary"],
+                user_args: args,
+            },
+            DefaultMaxCount(1),
+        )
+    }
+
+    pub(crate) fn ll(args: &[String]) -> GitResult {
+        debug!("_ll_ called with: {:#?}", args);
+
+        parse_for_max_count_and_execute(
+            GitCommand {
+                subcommand: "log",
+                default_args: &[
+                    COLOR,
+                    "--pretty='%C(yellow)%h %C(magenta)%as %C(blue)%aL %C(cyan)%s%C(reset)'",
+                ],
+                user_args: args,
+            },
             DefaultMaxCount(25),
         )
     }
@@ -87,18 +118,77 @@ impl Git {
     pub(crate) fn restore(args: &[String]) -> GitResult {
         debug!("_restore_ called with: {:#?}", args);
 
-        parse_for_all_files_and_execute("restore", &[], args)
+        execute_git_command(GitCommand {
+            subcommand: "restore",
+            default_args: &[],
+            user_args: args,
+        })
+    }
+
+    pub(crate) fn restore_all() -> GitResult {
+        debug!("_restore_all_ called");
+
+        execute_git_command(GitCommand {
+            subcommand: "restore",
+            default_args: &[":/"],
+            user_args: &[],
+        })
+    }
+
+    pub(crate) fn show(args: &[String]) -> GitResult {
+        debug!("_last_ called with: {:#?}", args);
+
+        parse_for_max_count_and_execute(
+            GitCommand {
+                subcommand: "show",
+                default_args: &[COLOR, "--expand-tabs=4"],
+                user_args: args,
+            },
+            DefaultMaxCount(1),
+        )
     }
 
     pub(crate) fn show_files(args: &[String]) -> GitResult {
         debug!("_show_files_ called with: {:#?}", args);
 
         parse_for_max_count_and_execute(
-            "show",
-            &["--pretty=", "--name-only"],
-            args,
+            GitCommand {
+                subcommand: "show",
+                default_args: &["--pretty=", "--name-only"],
+                user_args: args,
+            },
             DefaultMaxCount(1),
         )
+    }
+
+    pub(crate) fn undo(num: u8) -> GitResult {
+        debug!("undo called with: {:#?}", num);
+
+        execute_git_command(GitCommand {
+            subcommand: "reset",
+            default_args: &[&format!("HEAD~{}", num)],
+            user_args: &[],
+        })
+    }
+
+    pub(crate) fn unstage(args: &[String]) -> GitResult {
+        debug!("_unstage_ called with: {:#?}", args);
+
+        execute_git_command(GitCommand {
+            subcommand: "restore",
+            default_args: &["--staged"],
+            user_args: args,
+        })
+    }
+
+    pub(crate) fn unstage_all() -> GitResult {
+        debug!("_update_all_ called");
+
+        execute_git_command(GitCommand {
+            subcommand: "restore",
+            default_args: &["--staged", ":/"],
+            user_args: &[],
+        })
     }
 
     pub(crate) fn update(branch: &String) -> GitResult {
@@ -107,102 +197,84 @@ impl Git {
         if branch.is_empty() {
             Err(anyhow!("must provide a branch name"))
         } else {
-            execute_git_command("fetch", &["origin"], &[format!("{0}:{0}", branch)])
+            execute_git_command(GitCommand {
+                subcommand: "fetch",
+                default_args: &["origin"],
+                user_args: &[format!("{0}:{0}", branch)],
+            })
         }
-    }
-
-    pub(crate) fn unstage(args: &[String]) -> GitResult {
-        debug!("_unstage_ called with: {:#?}", args);
-
-        parse_for_all_files_and_execute("restore", &["--staged"], args)
-    }
-}
-
-/// parse first arg for specifier 'all' to operate on all files
-pub(crate) fn parse_for_all_files_and_execute(
-    command: &str,
-    default_args: &[&str],
-    user_args: &[String],
-) -> GitResult {
-    debug!(
-        "_parse_for_all_and_execute_ called with: {:#?}\n{:#?}\n{:#?}",
-        command, default_args, user_args
-    );
-
-    if user_args.is_empty() {
-        Err(anyhow!("must provide \"all\" or filename(s)"))
-    } else if user_args[0].to_uppercase() == "ALL" {
-        if user_args.len() > 1 {
-            Err(anyhow!("the argument \"all\" should be used alone"))
-        } else {
-            let default_args = &[default_args, &[":/"]].concat();
-            execute_git_command("restore", default_args, &user_args[1..])
-        }
-    } else {
-        execute_git_command("restore", &[], user_args)
     }
 }
 
 /// parse first user arg for use as value to `--max-count=`, otherwise use `default_max_count`
 fn parse_for_max_count_and_execute(
-    command: &str,
-    default_args: &[&str],
-    user_args: &[String],
+    command: GitCommand,
     default_max_count: DefaultMaxCount,
 ) -> Result<GitCommandResult> {
     debug!(
-        "_parse_for_max_count_and_execute_ called with: {:#?}\n{:#?}\n{:#?}\n{:#?}",
-        command, default_args, user_args, default_max_count
+        "_parse_for_max_count_and_execute_ called with: {:#?}\n{:#?}",
+        command, default_max_count
     );
 
-    let (max_count, user_args): (u8, &[String]) = match user_args.is_empty() {
-        true => (default_max_count.0, &user_args),
-        false => match user_args[0].parse::<u8>() {
-            Ok(num) => (num, &user_args[1..]),
-            Err(_) => (default_max_count.0, user_args),
+    let (max_count, user_args): (u8, &[String]) = match command.user_args.is_empty() {
+        true => (default_max_count.0, &command.user_args),
+        false => match command.user_args[0].parse::<u8>() {
+            Ok(num) => (num, &command.user_args[1..]),
+            Err(_) => (default_max_count.0, command.user_args),
         },
     };
 
-    let max_count = &format!("--max-count={}", max_count);
+    let max_count: &String = &format!("--max-count={}", max_count);
 
     // add --max_count argument to the end of original list of default_args
     let default_args: Vec<&str> = {
-        let mut args: Vec<&str> = default_args.to_vec();
+        let mut args: Vec<&str> = command.default_args.to_vec();
         args.push(max_count);
         args
     };
 
-    execute_git_command(command, &default_args, user_args)
+    execute_git_command(GitCommand {
+        subcommand: command.subcommand,
+        default_args: &default_args,
+        user_args,
+    })
 }
 
 /// Execute `git` command with the supplied arguments
-fn execute_git_command(command: &str, default_args: &[&str], user_args: &[String]) -> GitResult {
-    debug!(
-        "_execute_git_command_ called with: {:#?}\n{:#?}\n{:#?}",
-        command, default_args, user_args
-    );
+fn execute_git_command(command: GitCommand) -> GitResult {
+    debug!("_execute_git_command_ called with: {:#?}", command);
 
-    let mut command_args: Vec<&str> = Vec::new();
-    command_args.push(command);
+    let mut command_args: Vec<&str> = vec![command.subcommand];
 
-    if !default_args.is_empty() {
-        default_args.iter().for_each(|arg| command_args.push(arg));
-    }
-    if !user_args.is_empty() {
-        user_args.iter().for_each(|arg| command_args.push(arg));
+    if !command.default_args.is_empty() {
+        command
+            .default_args
+            .iter()
+            .for_each(|arg| command_args.push(arg));
     }
 
-    debug!("parsed command_args: {:#?}", command_args);
+    if !command.user_args.is_empty() {
+        command
+            .user_args
+            .iter()
+            .for_each(|arg| command_args.push(arg));
+    }
 
-    let output = Command::new("git")
-        .args(&command_args)
-        .output()
-        .with_context(|| {
-            format!(
-                "Failed to execute git command with args \"{}\"",
-                command_args.join(" ")
-            )
-        })?;
+    debug!("parsed command args: {:#?}", command_args);
+
+    let mut command = Command::new("git");
+    command.args(&command_args);
+
+    if PRINT_COMMAND.load(std::sync::atomic::Ordering::SeqCst) {
+        Print::blue_stderr(&format!("command: {:?}", command));
+    }
+
+    let output: std::process::Output = command.output().with_context(|| {
+        format!(
+            "Failed to execute git command with args \"{}\"",
+            command_args.join(" ")
+        )
+    })?;
 
     io::stdout().write_all(&output.stdout)?;
     io::stderr().write_all(&output.stderr)?;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use clap::Parser;
 use cli::{Cli, HookSubcommands, LogLevel, Subcommands};
-use git::{Git, GitCommandResult};
+use git::{Git, GitCommandResult, PRINT_COMMAND as GIT_PRINT_COMMAND};
 use log::{debug, LevelFilter};
 use print::Print;
+use std::sync::atomic::Ordering;
 
 mod cli;
 mod git;
@@ -33,6 +34,8 @@ fn main() -> ! {
 
     debug!("parsed Cli: {:#?}", &cli);
 
+    GIT_PRINT_COMMAND.store(cli.print_command, Ordering::Relaxed);
+
     #[allow(unused_variables)]
     let result = match &cli.subcommand {
         Subcommands::Aac { args } => todo!(),
@@ -45,11 +48,27 @@ fn main() -> ! {
         Subcommands::Hook { hook } => run_hook(hook),
         Subcommands::Files { args } => Git::show_files(args),
         Subcommands::Ll { args } => Git::ll(args),
-        Subcommands::Last { args } => todo!(),
-        Subcommands::Show { args } => todo!(),
-        Subcommands::Restore { args } => Git::restore(args),
-        Subcommands::Undo { args } => todo!(),
-        Subcommands::Unstage { args } => Git::unstage(args),
+        Subcommands::Last { args } => Git::last(args),
+        Subcommands::Show { args } => Git::show(args),
+        Subcommands::Restore { files } => {
+            if let Some(which) = files.which {
+                match which {
+                    cli::WhichFiles::All => Git::restore_all(),
+                }
+            } else {
+                Git::restore(&files.args)
+            }
+        }
+        Subcommands::Undo { num } => Git::undo(*num),
+        Subcommands::Unstage { files } => {
+            if let Some(which) = files.which {
+                match which {
+                    cli::WhichFiles::All => Git::unstage_all(),
+                }
+            } else {
+                Git::unstage(&files.args)
+            }
+        }
         Subcommands::Update { args } => Git::update(args),
     };
 
