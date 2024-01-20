7f6f72756caede3dcb4824926fe1a18f396db6c0
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-01-20 12:39:10 -0600

    test.sh: fix typo in command

--- test.sh
+++ test.sh
@@ -8,4 +8,4 @@ scriptdir=$(dirname -- "$(readlink -f -- "$0")")
 util="$scriptdir"/target/debug/git-util
 
 echo GIT_UTIL_USER_EMAIL='397636+ccb012100@users.noreply.github.com' "$util" --verbose --print-command hook precommit
-GIT_UTIL_USER_EMAIL='397636+ccb012100@users.noreply.github.com' $util --verbose --print-command hook precommit
+GIT_UTIL_USER_EMAIL='397636+ccb012100@users.noreply.github.com' $util --verbose --print-command hook pre-commit
