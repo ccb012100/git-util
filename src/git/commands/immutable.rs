3a15c6338e8f596c681db452709eeaea039ab0ae
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-02-02 16:40:33 -0600

    one_line_log: trim quote marks from log lines

--- src/git/commands/immutable.rs
+++ src/git/commands/immutable.rs
@@ -110,7 +110,7 @@ impl ImmutableCommands {
     pub fn one_line_log(num: Option<u16>, args: &[String]) -> GitResult {
         trace!("log_oneline() called with: {:#?}", num);
 
-        GitCommand {
+        let log_output: Output = GitCommand {
             subcommand: "log",
             default_args: &[
                 "--pretty='%C(yellow)%h %C(magenta)%as %C(blue)%aL %C(cyan)%s%C(reset)'",
@@ -118,7 +118,36 @@ impl ImmutableCommands {
             ],
             user_args: args,
         }
-        .execute_git_command()
+        .construct_git_command()
+        .output()
+        .with_context(|| "Failed to execute 'git log' command")?;
+
+        match log_output.status.success() {
+            true => {
+                let log_output_string = String::from_utf8(log_output.stdout)?;
+                let log_lines = log_output_string.lines();
+
+                let mut trimmed_log_output: String = String::new();
+
+                // Since git thinks this is not a tty, it wraps the log lines in single quotes; we remove them here.
+                for line in log_lines.into_iter() {
+                    trimmed_log_output.push_str(line.trim_matches('\''));
+                    trimmed_log_output.push('\n');
+                }
+
+                println!("{}", trimmed_log_output.trim_end());
+
+                io::stderr().write_all(&log_output.stderr)?;
+
+                Ok(GitCommandResult::Success)
+            }
+            false => {
+                io::stdout().write_all(&log_output.stdout)?;
+                io::stderr().write_all(&log_output.stderr)?;
+
+                Ok(GitCommandResult::Error)
+            }
+        }
     }
 
     /// `git show --expand-tabs=4 --max-count=NUM ARGS`
